f0ee764b8bebd2f8116f0f3d184ce6c2ae13318a
mibtesamarif <m.ibtesam.arif17@gmail.com>
2024-03-26 07:34:39 +0500

    slicing

---
+++ adminPanel/html/pages-misc-under-maintenance.php
@@ -0,0 +1,59 @@
+<?php
+include('header.php');
+?>
+
+  <body>
+    <!-- Content -->
+
+    <!--Under Maintenance -->
+    <div class="container-xxl container-p-y">
+      <div class="misc-wrapper">
+        <h2 class="mb-2 mx-2">Under Maintenance!</h2>
+        <p class="mb-4 mx-2">Sorry for the inconvenience but we're performing some maintenance at the moment</p>
+        <a href="index.html" class="btn btn-primary">Back to home</a>
+        <div class="mt-4">
+          <img
+            src="../assets/img/illustrations/girl-doing-yoga-light.png"
+            alt="girl-doing-yoga-light"
+            width="500"
+            class="img-fluid"
+            data-app-dark-img="illustrations/girl-doing-yoga-dark.png"
+            data-app-light-img="illustrations/girl-doing-yoga-light.png"
+          />
+        </div>
+      </div>
+    </div>
+    <!-- /Under Maintenance -->
+
+    <!-- / Content -->
+
+    <div class="buy-now">
+      <a
+        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+        target="_blank"
+        class="btn btn-danger btn-buy-now"
+        >Upgrade to Pro</a
+      >
+    </div>
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
